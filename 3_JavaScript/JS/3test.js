5fbaa2116956da9102eff5d54b7c3f4675de572c
TGB98 <rudwp1018@gmail.com>
2024-08-28 17:51:26 +0900

    tag적용시키기해야됨

--- 3_JavaScript/JS/3test.js
+++ 3_JavaScript/JS/3test.js
@@ -1,4 +1,3 @@
-const tag = document.getElementById('tagName'); // 태그 요소 얻어오기
 const input = document.getElementById('tagInnerText'); // input 요소 얻어오기
 const color = document.getElementById('colorName'); // 폰트 색상 input
 
@@ -8,12 +7,13 @@ document.querySelector('#btn').addEventListener("click", ()=>{
 // const input = document.getElementById('tagInnerText'); // input 요소 얻어오기
 // const color = document.getElementById('colorName'); // 폰트 색상 input
 
-  const tagName = tag.value;
+  const tag = document.getElementById('tagName').value;
   const text = input.value;
   const fontColor = color.value;
 
   const box = document.querySelector('#result');
   box.innerText = `${text}`;
   box.style.color = fontColor;
-  
+  const tagName = document.createElement(tag);
+  box.append(tagName);
 });
